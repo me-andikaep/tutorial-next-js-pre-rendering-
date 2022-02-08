46fc0d74634286b7c278092575c66b43ff42bdb1
andika <andika.ekaputra@dejavuexpress.com>
2022-02-08 16:13:28 +0700

    1st

---
+++ pages/users.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import User from '../components/user';
+
+function userList({ users }) {
+	return (
+		<>
+			<h1>List of users</h1>
+			{users.map((user) => {
+				return (
+					<div key={user.id}>
+						<User user={user} />
+					</div>
+				);
+			})}
+		</>
+	);
+}
+
+export default userList;
+
+export async function getStaticProps() {
+	const response = await fetch('https://jsonplaceholder.typicode.com/users');
+	const data = await response.json();
+
+	// console.log(data);
+
+	return {
+		props: {
+			users: data,
+		},
+	};
+}
